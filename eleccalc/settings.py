f339a41531a584d729dd0cf6f39611e11ad99ffe
AlexHls <70367168+AlexHls@users.noreply.github.com>
2022-05-27 00:50:54 +0200

    User system (#8)
    * Updated email in legalnotice
    
    * Added login system & lock advanced sampling
    
    * Removed .vscode from git tracking

--- eleccalc/settings.py
+++ eleccalc/settings.py
@@ -146,3 +146,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Login
+LOGIN_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "/"
--- lectureformat/views.py
+++ lectureformat/views.py
@@ -23,7 +23,6 @@ from .models import *
 def home(request):
     return render(request, "home.html")
 
-
 def about(request):
     return render(request, "about.html")
 
